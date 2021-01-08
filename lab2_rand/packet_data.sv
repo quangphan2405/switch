39f65bd99cf2c97c919b990e418ec0f198e5c156
quangphan2405 <quangphan240599@gmail.com>
2021-01-08 22:39:28 +0200

    Initialize lab2

---
+++ lab2_rand/packet_data.sv
@@ -0,0 +1,58 @@
+/*-----------------------------------------------------------------
+File name     : packet_data.sv
+Developers    : Brian Dickinson
+Created       : 01/08/19
+Description   : lab1 packet data item 
+Notes         : From the Cadence "Essential SystemVerilog for UVM" training
+-------------------------------------------------------------------
+Copyright Cadence Design Systems (c)2019
+-----------------------------------------------------------------*/
+ 
+// add print and type policies here
+typedef enum bit[1:0] {
+		       ANY = 2'b00,
+		       SINGLE = 2'b01,
+		       MULTICAST = 2'b10,
+		       BROADCAST = 2'b11
+		       } ptype_t;
+
+typedef enum {HEX, DEC, BIN} format_t;
+
+// packet class
+class packet;
+
+   // add properties here
+   local string name;
+   bit [3:0] 	target, source;
+   bit [7:0] 	data;
+   ptype_t ptype;   
+
+   // add constructor to set instance name and source by arguments and packet type
+   function new(input string name_i,
+		input int    source_i);
+      name   = name_i;
+      source = 4'b1 << source_i;
+      ptype  = ANY;
+   endfunction : new
+
+   function string gettype();
+      return ptype.name();
+   endfunction : gettype
+
+   function string getname();
+      return name;
+   endfunction : getname
+
+   // add print with policy
+   function void print(input format_t format = HEX);
+      $display("Current packet properties with format: %s", format.name());
+      $display("Name: %s, packet type: %s", getname(), gettype());
+      case ( format )
+	DEC    : $display("Source: %d, target: %d, data: %d", source, target, data);
+	BIN    : $display("Source: %b, target: %b, data: %b", source, target, data);
+	default: $display("Source: %h, target: %h, data: %h", source, target, data);
+      endcase
+   endfunction : print
+   
+endclass
+
---
+++ lab2_rand/packet_test.sv
@@ -0,0 +1,60 @@
+/*-----------------------------------------------------------------
+File name     : packet_test.sv
+Developers    : Brian Dickinson
+Created       : 01/08/19
+Description   : lab1  module for testing packet data
+Notes         : From the Cadence "Essential SystemVerilog for UVM" training
+-------------------------------------------------------------------
+Copyright Cadence Design Systems (c)2019
+-----------------------------------------------------------------*/
+
+module packet_test;
+   // Import package
+   import packet_pkg::*;
+
+   // Create class handle
+   packet p1;
+
+   // Test block
+   initial begin : TEST
+      p1 = new("My package", 2);
+      p1.data = 8'h4a;
+      p1.target = 4'hc;
+      p1.print(BIN);
+      p1.print(DEC);
+      p1.print();
+   end
+
+//--------------------validate functions for verification --------------------
+//--------------------Do not edit below this line          --------------------
+
+function int countones (input logic[3:0] vec);
+  countones = 0;
+  foreach (vec [i])
+    if (vec[i]) countones++;
+endfunction
+
+function void validate (input packet ap);
+  int sco, tco;
+  sco = countones(ap.source);
+  tco = countones(ap.target);
+  if (sco != 1) 
+     $display("ERROR in source %h - no. bits set = %0d", ap.source, sco);
+  if (ap.ptype == "broadcast") begin
+    if  (ap.target != 4'hf) 
+       $display("ERROR: broadcast packet target is %h not 4'hf", ap.target);
+  end
+  else 
+  begin
+    if ( |(ap.source & ap.target) == 1'b1)   
+      $display("ERROR: non-broadcast packet %s has same source %h and target %h bit set", ap.getname(), ap.source, ap.target);
+    if ((ap.ptype == "single") & (tco != 1)) 
+      $display("ERROR: single packet %s does not have only one bit set in target %h", ap.getname(), ap.target);
+    if ((ap.ptype == "multicast") & (tco < 2)) 
+      $display("ERROR: multicast packet %s does not have more than one bit set in target %h", ap.getname(), ap.target);
+  end
+endfunction
+    
+endmodule
+   
+
